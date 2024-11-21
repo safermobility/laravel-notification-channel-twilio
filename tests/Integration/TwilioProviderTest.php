5cef1deaecb412b72ba9face076c419e20c05173
Pascal Baljet <pascal@protone.media>
2024-11-21 12:25:37 +0100

    Updated and improved `TwilioProviderTest`

--- tests/Integration/TwilioProviderTest.php
+++ tests/Integration/TwilioProviderTest.php
@@ -7,17 +7,22 @@ namespace NotificationChannels\Twilio\Tests\Integration;
 use NotificationChannels\Twilio\Exceptions\InvalidConfigException;
 use NotificationChannels\Twilio\Tests\IntegrationTestCase;
 use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioConfig;
+use NotificationChannels\Twilio\TwilioProvider;
+use Twilio\Rest\Client;
 
 class TwilioProviderTest extends IntegrationTestCase
 {
-    public function testThatApplicationCannotCreateChannelWithoutConfig()
+    /** @test */
+    public function it_cannot_create_the_application_without_config()
     {
         $this->expectException(InvalidConfigException::class);
 
         $this->app->get(TwilioChannel::class);
     }
 
-    public function testThatApplicationCannotCreateChannelWithoutSid()
+    /** @test */
+    public function it_cannot_create_the_application_without_sid()
     {
         $this->app['config']->set('twilio-notification-channel.username', 'test');
         $this->app['config']->set('twilio-notification-channel.username', 'password');
@@ -26,12 +31,23 @@ class TwilioProviderTest extends IntegrationTestCase
         $this->app->get(TwilioChannel::class);
     }
 
-    public function testThatApplicationCreatesChannelWithConfig()
+    /** @test */
+    public function it_can_create_the_application_with_sid()
     {
         $this->app['config']->set('twilio-notification-channel.username', 'test');
         $this->app['config']->set('twilio-notification-channel.password', 'password');
         $this->app['config']->set('twilio-notification-channel.account_sid', '1234');
 
         $this->assertInstanceOf(TwilioChannel::class, $this->app->get(TwilioChannel::class));
+    }
+
+    /** @test */
+    public function it_provides_three_classes()
+    {
+        $provides = (new TwilioProvider($this->app))->provides();
+
+        $this->assertTrue(in_array(TwilioChannel::class, $provides));
+        $this->assertTrue(in_array(TwilioConfig::class, $provides));
+        $this->assertTrue(in_array(Client::class, $provides));
     }
 }
